811bc6c8c71b2982a910fa25640bb5d4fce470b9
msiddiqurrehman <m.siddiqur.rehman@gmail.com>
2025-06-15 02:46:15 +0530

    404 page complete with fallback route.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,3 +18,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::fallback(function () {
+    return view('404');
+});
